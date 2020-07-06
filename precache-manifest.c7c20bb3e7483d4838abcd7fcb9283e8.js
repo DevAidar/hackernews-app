8d91d22f657774379aa2499961d407a0287f51cf
Lime Peach <limepeach@LimePeach.local>
2020-07-05 23:55:05 -0700

    Updates

---
+++ precache-manifest.c7c20bb3e7483d4838abcd7fcb9283e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8239d105a750b7fa339d294be752556",
+    "url": "/hacker-news-app/index.html"
+  },
+  {
+    "revision": "bbf383ccd8cbecef8664",
+    "url": "/hacker-news-app/static/css/main.ea79625f.chunk.css"
+  },
+  {
+    "revision": "501831d1c3210eb455a6",
+    "url": "/hacker-news-app/static/js/2.125a91f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news-app/static/js/2.125a91f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf383ccd8cbecef8664",
+    "url": "/hacker-news-app/static/js/main.911ea5bd.chunk.js"
+  },
+  {
+    "revision": "c35fdb72550f45e030ca",
+    "url": "/hacker-news-app/static/js/runtime-main.663cf605.js"
+  }
+]);
